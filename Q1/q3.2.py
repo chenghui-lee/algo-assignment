d07eafd942a6ac5ebb5264b2bf7663a95bcc8d26
jaising <js.tan@skybox.com.my>
2021-05-03 23:04:12 +0800

    add comment

--- Q1/q3.2.py
+++ Q1/q3.2.py
@@ -10,16 +10,19 @@ hub = ["City-link Express", "Pos Laju", "GDEX", "J&T", "DHL"]
 
 cus_distance = []
 
+# convert from dictionary to list
 for i in range(1,4):
     companyDistance = {}
     for j in range(0, len(hub)):
         companyDistance[hub[j]] = distance[('Customer '+str(i), hub[j])]
     cus_distance.append(companyDistance)
 
+# sort according to result
 for i in range(len(cus_distance)):
     cus_distance[i] = {k: v for k, v in sorted(cus_distance[i].items(), key=lambda item: item[1])}
 
 print(cus_distance)
 
+# print result
 for i in range(1,4):
     print('The best choice for customer ' + str(i), 'is ', list(cus_distance[i-1].items())[0])
